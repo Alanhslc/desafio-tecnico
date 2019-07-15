6581e181b4d884bdb66657ce80d200a3c34d6641
Alan Lucena <alanh.lucena@gmail.com>
2019-07-15 00:35:18 -0300

    Refatoracao api swapi / alteracoes testes unitarios

--- src/main/java/com/desafio/tecnico/integracao/SwapiService.java
+++ src/main/java/com/desafio/tecnico/integracao/SwapiService.java
@@ -1,20 +1,16 @@
 package com.desafio.tecnico.integracao;
 
-import static org.springframework.http.HttpMethod.GET;
-
 import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import com.desafio.tecnico.domains.PlanetaDomain;
-import com.desafio.tecnico.http.builder.HttpHeadersBuilder;
 import com.desafio.tecnico.integracao.dto.SwapiDTO;
 
 @Service
@@ -23,16 +19,15 @@ public class SwapiService {
 	private static final Logger LOGGER = LogManager.getLogger(SwapiService.class);
 	private static final String URL = "https://swapi.co/api/planets/";
 	
+	@Autowired
+	private RestTemplate restTemplate;
+	
 	public void obterAparicoesEmFilmePor(PlanetaDomain planeta) {
-		
-		RestTemplate restTemplate = new RestTemplate();
-		HttpHeaders headers = new HttpHeadersBuilder().build();
-        HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
         
         int aparicoesEmFilme = 0;
 
         try {
-        	SwapiDTO swapiDTO = restTemplate.exchange(URL + planeta.getId(), GET, entity, SwapiDTO.class).getBody();
+        	SwapiDTO swapiDTO = restTemplate.getForObject(URL + planeta.getId(), SwapiDTO.class);
         	aparicoesEmFilme = swapiDTO.getFilms().length;
 		} catch (HttpClientErrorException e) {
 			
--- src/test/java/com/desafio/tecnico/integracao/SwapiServiceTest.java
+++ src/test/java/com/desafio/tecnico/integracao/SwapiServiceTest.java
@@ -2,18 +2,28 @@ package com.desafio.tecnico.integracao;
 
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
 
 import com.desafio.tecnico.domains.PlanetaDomain;
+import com.desafio.tecnico.integracao.dto.SwapiDTO;
 
 @RunWith(SpringRunner.class)
 public class SwapiServiceTest {
 
+	private static final int ID_VALIDO = 1;
+	private static final int ID_INVALIDO = 193;
+
+	private static final String URL = "https://swapi.co/api/planets/";
+	
 	@TestConfiguration
     static class SwapiServiceTestContextConfiguration {
   
@@ -23,21 +33,46 @@ public class SwapiServiceTest {
         }
     }
 	
+	@Before
+	public void setup() {
+		this.planetaComAparicoesEmFilme = new SwapiDTO();
+		this.planetaComAparicoesEmFilme.setFilms(new String[] {"film1"});
+		
+		this.planetaSemAparicoesEmFilme = new SwapiDTO();
+		this.planetaSemAparicoesEmFilme.setFilms(new String[] {});
+	} 
+	
+	private SwapiDTO planetaComAparicoesEmFilme;
+	private SwapiDTO planetaSemAparicoesEmFilme;
+	
 	@Autowired
     private SwapiService swapiService;
 	
+	@MockBean
+	private RestTemplate restTemplate;
+	
 	@Test
 	public void deveObterPlanetaComAparicoesEmFilme() {
+		
+		Mockito.when(restTemplate.getForObject(URL + ID_VALIDO, SwapiDTO.class))
+			.thenReturn(this.planetaComAparicoesEmFilme);
+		
 		PlanetaDomain planeta = new PlanetaDomain();
-		planeta.setId(1);
+		planeta.setId(ID_VALIDO);
+		
 		swapiService.obterAparicoesEmFilmePor(planeta);
 		assertTrue(planeta.getAparicoesEmFilme() > 0);		
 	}
 	
 	@Test
 	public void deveObterPlanetaSemAparicoesEmFilme() {
+		
+		Mockito.when(restTemplate.getForObject(URL + ID_INVALIDO, SwapiDTO.class))
+			.thenReturn(this.planetaSemAparicoesEmFilme);
+		
 		PlanetaDomain planeta = new PlanetaDomain();
-		planeta.setId(193);
+		planeta.setId(ID_INVALIDO);
+		
 		swapiService.obterAparicoesEmFilmePor(planeta);
 		assertTrue(planeta.getAparicoesEmFilme() == 0);		
 	}
